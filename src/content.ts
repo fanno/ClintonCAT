c5b3860a11ffdd37584d7ced51a0c0681c376634
Morten Hundevad <fannoj@gmail.com>
2025-07-19 15:23:17 +0200

    mutation observer

--- src/content.ts
+++ src/content.ts
@@ -3,12 +3,62 @@ import browser from 'webextension-polyfill';
 
 DOMMessenger.registerMessageListener();
 
-const pageInfoPayload = {
-    domain: window.location.hostname,
-    url: window.location.href,
+const containerId = DOMMessenger.containerId();
+
+function sendPageInfo() {
+    const pageInfoPayload = {
+        domain: window.location.hostname,
+        url: window.location.href,
+    };
+
+    void browser.runtime.sendMessage({
+        type: 'pageInfo',
+        payload: pageInfoPayload,
+    });
+}
+
+sendPageInfo();
+
+let updateTimerRunnig = true;
+
+const observer = new MutationObserver((mutationsList) => {
+    let _ignore = false;
+    if (!updateTimerRunnig) {
+        mutationsList.forEach((record) => {
+            if (record.type === 'childList') {
+                record.addedNodes.forEach((node) => {
+                    if (node instanceof HTMLElement && node.id) {
+                        if (node.id == containerId) {
+                            _ignore = true;
+                        }
+                    }
+                });
+                record.removedNodes.forEach(() => {
+                    _ignore = true;
+                });
+            }
+        });
+
+        if (!_ignore) {
+            updateTimerRunnig = true;
+            console.log('MutationObserver::setTimeout::SET');
+            setTimeout(() => {
+                console.log('MutationObserver::setTimeout::DONE');
+                sendPageInfo();
+                updateTimerRunnig = false;
+            }, 5000);
+        }
+    }
+});
+
+const config = {
+    childList: true, // observe direct children being added/removed
+    subtree: true, // observe all descendants
+    attributes: false, // observe attribute changes
+    characterData: false, // observe text content changes
 };
 
-void browser.runtime.sendMessage({
-    type: 'pageInfo',
-    payload: pageInfoPayload,
-});
+setTimeout(() => {
+    updateTimerRunnig = false;
+    observer.observe(document.body, config);
+}, 20000);
